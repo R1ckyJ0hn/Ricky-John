41d0c8134a3331ce25286c778f417b908e6c0e0b
R1ckyJ0hn <101105185+R1ckyJ0hn@users.noreply.github.com>
2022-03-22 16:17:08 +0530

    Add files via upload

---
+++ Day 12 task/newmeth.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace day12
+{
+
+    [Serializable]
+    class Employeede
+    {
+
+        public string name = "Sid the great";
+        public int age = 25;
+        public string Empid = "ABC$321";
+        public string gender = "Male";
+
+    }
+    class newmeth
+    {
+        public void SeretoFile()
+        {
+            Employeede sobj = new Employeede();
+            FileStream fs = new FileStream("Empdets.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, sobj);
+            fs.Close();
+        }
+        public void Dserial()
+        {
+            FileStream fs = new FileStream("Empdets.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            Employeede stu = (Employeede) f.Deserialize(fs);
+            Console.WriteLine(stu.name);
+            Console.WriteLine(stu.age);
+            Console.WriteLine(stu.gender);
+            Console.WriteLine(stu.Empid);
+
+
+
+        }
+
+        public static void Main()
+        {
+            newmeth lobj = new newmeth();
+            lobj.SeretoFile();
+            lobj.Dserial();
+            Console.ReadLine();
+
+        }
+    }
+}
